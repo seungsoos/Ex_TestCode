521974e24e46ba719cbeaf0fa0115d60b5153dcb
seungsoo <seung_soos@naver.com>
2024-02-20 23:54:06 +0900

    feat : 주문 도메인 개발

---
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -0,0 +1,14 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    public void createOrder(OrderCreateRequest orderCreateRequest) {
+
+    }
+}
---
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -0,0 +1,34 @@
+package sample.cafekiosk.spring.domain.order;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProduct;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredDateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+
+}
