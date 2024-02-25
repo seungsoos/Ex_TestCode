6f8d5362145016680fd2996fcd364cd0d63aada5
seungsoo <seung_soos@naver.com>
2024-02-25 23:08:29 +0900

    feat : 주문 셍상시 중복 주문 처리가능 로직 개선 ex) 아메리카노 2잔

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.service.order.OrderService;
 import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 
 import java.time.LocalDateTime;
 
@@ -16,8 +17,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public void createOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
+    public OrderResponse createOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
         LocalDateTime now = LocalDateTime.now();
-        orderService.createOrder(orderCreateRequest, now);
+        return orderService.createOrder(orderCreateRequest, now);
     }
 }
--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,18 +1,17 @@
 package sample.cafekiosk.spring.api.service.order;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
-import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
 import java.time.LocalDateTime;
@@ -21,7 +20,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
-import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.STOP_SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.BAKERY;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
@@ -33,8 +31,18 @@ class OrderServiceTest {
     @Autowired
     private OrderService orderService;
     @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private OrderProductRepository orderProductRepository;
 
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+    }
 
     @Test
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
@@ -64,6 +72,38 @@ class OrderServiceTest {
                 .containsExactlyInAnyOrder(
                         tuple("001", 4000),
                         tuple("002", 4500)
+                );
+
+    }
+
+    @Test
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성 할 수 있다.")
+    void createOrderWithDuplicateProductNumbers() {
+        // given
+        Product product1 = createProduct(HANDMADE, "001", 4000);
+        Product product2 = createProduct(BAKERY, "002", 4500);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+        LocalDateTime now = LocalDateTime.now();
+        // when
+        OrderResponse orderResponse = orderService.createOrder(orderCreateRequest, now);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(now, 8000);
+
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 4000),
+                        tuple("001", 4000)
                 );
 
     }
